ad094a857e3aa76d86ad29a43ffd144f4219f394
T45K <tasktas9@gmail.com>
2021-01-28 21:11:11 +0900

    aggressive

--- app/src/main/kotlin/io/github/t45k/ninja/usecase/preprocess/LexicalAnalyzer.kt
+++ app/src/main/kotlin/io/github/t45k/ninja/usecase/preprocess/LexicalAnalyzer.kt
@@ -1,7 +1,7 @@
 package io.github.t45k.ninja.usecase.preprocess
 
-import io.github.t45k.ninja.entity.NGrams
 import org.eclipse.jdt.core.ToolFactory
+import org.eclipse.jdt.core.compiler.ITerminalSymbols
 import org.eclipse.jdt.core.compiler.ITerminalSymbols.TokenNameEOF
 
 /**
@@ -9,12 +9,28 @@ import org.eclipse.jdt.core.compiler.ITerminalSymbols.TokenNameEOF
  * This class is used when you want to filter code blocks by min_tokens.
  */
 class LexicalAnalyzer {
-    fun analyze(text: String): List<Int> =
-        ToolFactory.createScanner(false, false, false, "14")
+    fun analyze(text: String): List<Int> {
+        val tokenSequence = ToolFactory.createScanner(false, false, false, "14")
             .also { it.source = text.toCharArray() }
             .let { scanner ->
                 generateSequence { 0 }
                     .map { scanner.nextToken }
                     .takeWhile { it != TokenNameEOF }
             }.toList()
+        return mutableListOf<Int>()
+            .apply {
+                var start = 0
+                for (end in tokenSequence.indices) {
+                    if (tokenSequence[end] == ITerminalSymbols.TokenNameLBRACE ||
+                        tokenSequence[end] == ITerminalSymbols.TokenNameRBRACE ||
+                        tokenSequence[end] == ITerminalSymbols.TokenNameSEMICOLON
+                    ) {
+                        if (start != end) {
+                            add(tokenSequence.subList(start, end).hashCode())
+                        }
+                        start = end + 1
+                    }
+                }
+            }
+    }
 }
